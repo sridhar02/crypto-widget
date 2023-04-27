07f1e4bfb77cd5891e723951d510f73664706eb2
sridhar02 <kattasridhar02@gmail.com>
2023-04-27 12:21:15 +0530

    :sparkles: add sparkline graph

--- src/components/CryptoTracker.js
+++ src/components/CryptoTracker.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
+import { Sparklines, SparklinesLine, SparklinesSpots } from "react-sparklines";
 
 const symbolToCoinGeckoId = {
   pepe: "pepe",
@@ -60,26 +61,44 @@ const CryptoTicker = ({ symbol }) => {
               </h3>
               <p>{data.id}</p>
             </div>
-            <p>${data.current_price.toFixed(2)}</p>
+            <div>
+              <p>${data.current_price.toFixed(2)}</p>
+              <div></div>
+            </div>
           </div>
-          <div className="border-b-2">
-            <div className="flex justify-between mb-1">
-              <span>Market Cap:</span>
-              <span>${data.market_cap.toLocaleString()}</span>
+          <div className="border-b-2 flex ">
+            <div>
+              <div className="flex justify-between mb-1">
+                <span>Market Cap: </span>
+                <span>${data.market_cap.toLocaleString()}</span>
+              </div>
+              <div className="flex justify-between mb-1">
+                <span>Volume: </span>
+                <span>${data.total_volume.toLocaleString()}</span>
+              </div>
+              <div className="flex justify-between mb-1">
+                <span>Day range: </span>
+                <span>
+                  ${data.low_24h.toFixed(8)} - ${data.high_24h.toFixed(8)}
+                </span>
+              </div>
             </div>
-            <div className="flex justify-between mb-1">
-              <span>Volume:</span>
-              <span>${data.total_volume.toLocaleString()}</span>
-            </div>
-            <div className="flex justify-between mb-1">
-              <span>Day range:</span>
-              <span>
-                ${data.low_24h.toFixed(8)} - ${data.high_24h.toFixed(8)}
-              </span>
+            <div className="mt-2">
+              <Sparklines
+                data={data.sparkline_in_7d.price}
+                width={100}
+                height={20}
+              >
+                <SparklinesLine color="blue" />
+                <SparklinesSpots
+                  size={4}
+                  spotColors={{ 0: "red", "-1": "green" }}
+                />
+              </Sparklines>
             </div>
           </div>
 
-          <a href="#" className="bg-blue-600">
+          <a href="#" className="text-blue-600">
             More about {data.name.toUpperCase() + "/USD"}
           </a>
         </div>
